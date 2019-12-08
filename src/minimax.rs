c505ed6f6ed1745b9ee7d799dc4193377da9c44a
Robert <sk.robchik@gmail.com>
2019-12-07 21:46:19 -0800

    changed terminality for option of player

--- src/minimax.rs
+++ src/minimax.rs
@@ -1,29 +1,23 @@
-struct NodeTerminality{
-    is_terminal: bool,
-    winning_player: Player,
-}
-
-struct Evaluation{
+pub struct Evaluation{
     depth: i32,
     value: f32,
 }
 
-enum Player{
+pub enum Player{
     MIN,
     MAX,
 }
 
-fn minimax<N, FN, IN, FT, FE>(node: N, depth: i32, successors: &FN, terminal: &FT, evaluation: &FE, player: Player) -> f32
+pub fn minimax<N, FN, IN, FT, FE>(node: &N, depth: i32, successors: &FN, terminal: &FT, evaluation: &FE, player: Player) -> f32
 where
-    N: Eq + Clone,
     FN: Fn(&N) -> IN,
     IN: IntoIterator<Item = N>,
-    FT: Fn(&N) -> NodeTerminality,
+    FT: Fn(&N) -> Option<Player>,
     FE: Fn(&N) -> i32,
 {
     let terminality = terminal(&node);
-    if terminality.is_terminal {
-        return match terminality.winning_player{
+    if terminality.is_some() {
+        return match terminality.unwrap(){
             Player::MIN => -std::f32::INFINITY,
             Player::MAX => std::f32::INFINITY,
         }
@@ -34,7 +28,7 @@ where
             Player::MAX => -std::f32::INFINITY,
         };
         for child in successors(&node){
-            let child_val = minimax(child, depth-1, successors, terminal, evaluation, match player{
+            let child_val = minimax(&child, depth-1, successors, terminal, evaluation, match player{
                 Player::MIN => Player::MAX,
                 Player::MAX => Player::MIN,
             });
