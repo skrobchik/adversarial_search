99dddca974b9cc6303e37aa1114165e6e94ee37e
Robert <sk.robchik@gmail.com>
2019-12-09 12:51:00 -0800

    changed input argument of player to bool

--- src/minimax.rs
+++ src/minimax.rs
@@ -3,18 +3,29 @@ pub struct Evaluation{
     value: f32,
 }
 
-pub enum Player{
+enum Player{
     MIN,
     MAX,
 }
 
-pub fn minimax<N, FN, IN, FT, FE>(node: &N, depth: i32, successors: &FN, terminal: &FT, evaluation: &FE, player: Player) -> f32
+fn is_maximizing_player(player: Player) -> bool{
+    match player{
+        Player::MAX => true,
+        Player::MIN => false,
+    }
+}
+
+pub fn minimax<N, FN, IN, FT, FE>(node: &N, depth: i32, successors: &FN, terminal: &FT, evaluation: &FE, maximizing_player: bool) -> f32
 where
     FN: Fn(&N) -> IN,
     IN: IntoIterator<Item = N>,
     FT: Fn(&N) -> Option<f32>,
     FE: Fn(&N) -> i32,
 {
+    let player = match maximizing_player{
+        true => Player::MAX,
+        false => Player::MIN,
+    };
     let terminality = terminal(&node);
     if terminality.is_some() {
         return terminality.unwrap();
@@ -25,10 +36,7 @@ where
             Player::MAX => -std::f32::INFINITY,
         };
         for child in successors(&node){
-            let child_val = minimax(&child, depth-1, successors, terminal, evaluation, match player{
-                Player::MIN => Player::MAX,
-                Player::MAX => Player::MIN,
-            });
+            let child_val = minimax(&child, depth-1, successors, terminal, evaluation, is_maximizing_player(player));
             let is_better_value: bool = match player{
                 Player::MIN => child_val < val,
                 Player::MAX => child_val > val,
